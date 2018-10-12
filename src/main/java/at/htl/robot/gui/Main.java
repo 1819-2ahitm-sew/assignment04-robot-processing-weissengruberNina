d8b936a37b9270f868f9aaf9df9598978934ba1d
weissengruberNina <n.weissengruber@students.htl-leonding.ac.at>
2018-10-12 10:42:32 +0200

    ende der programierstunde programm noch nicht fertig

--- src/main/java/at/htl/robot/gui/Main.java
+++ src/main/java/at/htl/robot/gui/Main.java
@@ -9,6 +9,10 @@ import processing.core.PApplet;
 public class Main extends PApplet {
 
     // Hier die Member-Attribute eintragen
+    int upperMargin = 50;
+    int leftMargin = 50;
+    int boxLength = 50;
+    Robot robot;
 
     public static void main(String[] args) {
         PApplet.main("at.htl.robot.gui.Main", args);
@@ -20,6 +24,9 @@ public class Main extends PApplet {
 
     public void setup() {
         background(209); //https://processing.org/tutorials/color/
+        robot = new Robot();
+        robot.setX(1);
+        robot.setY(1);
 
 
     }
@@ -28,6 +35,26 @@ public class Main extends PApplet {
      * Diese Methode wird iterativ durchlaufen (wie loop() beim Arduino)
      */
     public void draw() {
+        for (int i = 0; i < 11; i++) {
+            line(leftMargin,
+                 upperMargin + i * boxLength,
+                 leftMargin + 10 * boxLength,
+                 upperMargin + i *boxLength);
+
+            line(leftMargin + i * boxLength,
+                    upperMargin,
+                    leftMargin + i * boxLength,
+                    upperMargin + 10 *boxLength);
+
+            int boxCenterX = leftMargin- boxLength/2 +robot.getX()*boxLength;
+            int boxCenterY = upperMargin- boxLength/2 +robot.getY()*boxLength;
+
+            ellipse(boxCenterX,
+                    boxCenterY,
+                    (int) (boxLength * 0.8),
+                    (int) (boxLength* 0.8));
+
+        }
 
     }
 
@@ -47,7 +74,7 @@ public class Main extends PApplet {
      * Erstellen Sie eine eigene Methode zum Löschen des Bildschirms
      */
     public void deleteAll() {
-
+        background(209);
     }
 
     /**
@@ -57,9 +84,12 @@ public class Main extends PApplet {
         println("pressed " + key + " " + keyCode);
 
         if (key == 'f' || key == 'F') {
-
+            if (0 < robot.getY() && robot.getY() < 10 && 0 < robot.getX() &&robot.getX() <10) {
+                deleteAll();
+                robot.stepForward();
+            }
         } else if (key == 'l' || key == 'L') {
-
+            robot.rotateLeft();
         }
 
     }
